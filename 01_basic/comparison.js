5e3c2ee4c081fe4251d0b16af78cb76690c2a8f9
Ashiv Kumar <131641937+ashiv2001Kumar@users.noreply.github.com>
2025-03-19 11:11:53 +0000

    comparisons

---
+++ 01_basic/comparison.js
@@ -0,0 +1,15 @@
+// console.log('2' > 1)
+// console.log("02" > 1)
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+/* the reason is that an quality check == and 
+comaparison >, <, >=, <=, works differently ,
+comapriosion convert null to a number treating it as 0
+that why  null >= 0 is true and null > 0 is false
+*/
+
+console.log("2" === 2);
+//  here ===  compare the value also checking the data type o
