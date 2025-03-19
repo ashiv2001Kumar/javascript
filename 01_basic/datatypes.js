dd82d381bda17636c4d7b397f57d287334dea37f
Ashiv Kumar <131641937+ashiv2001Kumar@users.noreply.github.com>
2025-03-19 07:54:32 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as newer version
+
+// alert( 3 + 3)// we are using node.js,not browser
+
+console.log(3 + 3)
+// code readibility should be high
+
+console.log('abs')
+
+// number => 2 tp power 53
+// string => ""
+// boolean => true /false
+// null => standlone 
+// undefined => value not defined mean not given value
+// symbol => uniqueness and immutable // non string value
+
+// objects
+
+console.log(typeof "ashiv")
+console.log(typeof undefined) //undfined
+console.log(typeof null)//object
