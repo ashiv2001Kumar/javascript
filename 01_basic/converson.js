8fc27222765acebdd2c8da123d186ff2cca32ca5
Ashiv Kumar <131641937+ashiv2001Kumar@users.noreply.github.com>
2025-03-19 10:50:41 +0000

    conversions

---
+++ 01_basic/converson.js
@@ -0,0 +1,57 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = "ashiv"
+let score = true
+
+
+
+
+/* here are the two type of methods
+ to know the type of  variable
+*/
+// console.log(typeof score)
+// console.log(typeof(score))//with method
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// "33" => easy covert into number
+// if "33abc"=> NaN
+// true => 1 ,false=>0
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+let isLoggedIn = "ashiv"
+
+
+let booleanLog = Boolean(isLoggedIn)
+// console.log(booleanLog)
+
+// 1 ==> True / or 0 ==> false
+//" " ==> false / or " ashiv " == > True
+
+
+let number = 33
+
+let stringNumber = String(number)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ***********operators****************************
+
+let str1 = "ashiv"
+let str2 = " choudhary"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 4 + 3)
+// console.log(1 + 2 + "5")
+
+console.log(true)
+console.log(+true)
+console.log(+"")
