6e11da556b4ee4aecc6f8c1d1a088348b898e1b7
Ashiv Kumar <131641937+ashiv2001Kumar@users.noreply.github.com>
2025-03-25 12:29:49 +0000

    switch.js

---
+++ control/switch.js
@@ -0,0 +1,38 @@
+// synatx
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const Day = 4
+switch (Day){
+    case 1:
+        console.log(1,"Mon")
+        break;
+    case 2:
+        console.log(2,"Tue")
+        break;
+    case 3:
+        console.log(3,"Wed")
+        break;
+    case 4:
+        console.log(4,"Thu")
+        break;
+    case 5:
+        console.log(5,"Fri")
+        break;
+    case 6:
+        console.log(6,"Sat")
+        break;
+    case 7:
+        console.log(7,"Sun")
+        break;
+
+    default:
+        console.log('default case')
+        break;
+}
