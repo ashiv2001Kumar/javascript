af341b9600c20012ba705e68504a7aea24cfc331
Ashiv Kumar <131641937+ashiv2001Kumar@users.noreply.github.com>
2025-03-25 06:53:53 +0000

    function2

--- fun/funpara_1.js
+++ fun/funpara_1.js
@@ -1,37 +1,36 @@
+// // function addnumber (number1 , number2){
+// //    console.log(number1 + number2)
+// // }
+// // addnumber(3,5)
+// // -------------------------------------------
+
 // function addnumber (number1 , number2){
-//    console.log(number1 + number2)
-
+//    // let result = number1 + number2
+//    // return result
+//    return  number1 + number2
 // }
-// addnumber(3,5)
-// -------------------------------------------
+// const result = addnumber(3,5)
+// // console.log("Result:",result)
 
-function addnumber (number1 , number2){
-   // let result = number1 + number2
-   // return result
-   return  number1 + number2
-}
-const result = addnumber(3,5)
-// console.log("Result:",result)
+// // -------------------------------------------------
+// function loginuser(username){
+//    return `${ username},just login`
+// }
+// // console.log(loginuser("ashiv"))
 
-// -------------------------------------------------
-function loginuser(username){
-   return `${ username},just login`
-}
-// console.log(loginuser("ashiv"))
+// // -------------------------------------
+// function loginuser(username){
+//    // if(username == undefined)
+//       if (!undefined){
+//       console.log("please enter a username")
+//       return
+//    }
+//    return `${ username},just login`
+// }
+// // console.log(loginuser())
+// // ---------------------------------------
 
-// -------------------------------------
-function loginuser(username){
-   // if(username == undefined)
-      if (!undefined){
-      console.log("please enter a username")
-      return
-   }
-   return `${ username},just login`
-}
-// console.log(loginuser())
-// ---------------------------------------
-
-// --------------------function/Array/objects----------------
+// // --------------------function/Array/objects----------------
 
 function add (...num1){//rest operator
    return num1
@@ -40,25 +39,58 @@ function add (...num1){//rest operator
 
 
 
-const user = {
-   name : "ashiv",
-   price : 354
-}
+// const user = {
+//    name : "ashiv",
+//    price : 354
+// }
 
-function handleobject(anyobject){
-   console.log(` Username is ${anyobject.name} and price is ${anyobject.price} `)  
-}
+// function handleobject(anyobject){
+//    console.log(` Username is ${anyobject.name} and price is ${anyobject.price} `)  
+// }
 // handleobject(user)
+//---------------------------------------------------
+// // handleobject({
+// //    name:"Rubal",
+// //    price: 2345
+// // })
+
+// const newarray = [200,900,560,800]
+// function returnSecondvalue(getArray){
+//    return getArray[2]
+
+// }
+// console.log(returnSecondvalue(newarray))
+
+// const newarray = [200,900,560,800]
+// function returnSecondvalue(getArray){
+//    return getArray[2]
+// }
+// console.log(returnSecondvalue(newarray))
 
 
-// handleobject({
-//    name:"Rubal",
-//    price: 2345
-// })
 
-const newarray = [200,900,560,800]
-function returnSecondvalue(getArray){
-   return getArray[2]
-
+// const User = {
+//    name:"ashiv",
+//    price:47
+// }
+function handle(any){
+   console.log(`username is ${any.name}, and price of machine is ${any.price}`)
 }
-console.log(returnSecondvalue(newarray))
+// handle(User)
+
+// handle({name:"rubal",price:98})
+
+/*---------method1------------------*/
+
+// const newarray= [200,3000,490,950,900]
+// function getvalue(getnew){
+//    return getnew[3]
+// }
+// console.log(getvalue(newarray))
+
+/*-------------method2------------------- */
+const newarray= [200,3000,490,950,900]
+ function getvalue(getnew){
+   console.log(getnew[2])
+ }
+ getvalue(newarray)
