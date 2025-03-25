3976aac678e8717beb08c2cb0c6523b0a86f5cff
Ashiv Kumar <131641937+ashiv2001Kumar@users.noreply.github.com>
2025-03-25 11:16:53 +0000

    arrow fun

---
+++ fun/arrowfun.js
@@ -0,0 +1,72 @@
+const user = {
+    name: "Ashiv",
+    price : 344,
+    welcome_message : function(){
+        // console.log(`${this.name}, welcome user message`)
+        // console.log(this)// this keyword refer to the current context 
+        //context  what this value tells about
+    } 
+}
+// user.welcome_message()
+// user.name = "sam"
+// user.welcome_message()
+// console.log(this)
+
+
+
+
+// function one(){
+//     const username = "Ashiv"
+//     console.log(this.username)
+// }
+// one()
+
+// const one = function(){
+//     let username = "ashiv"
+//     console.log(this)
+// }
+// one()
+
+//-----------------------arrow fun -------------------------------
+// const addtwo = (num1,num2 ) => {
+//     return num1 + num2
+// }
+const addtwo = (num1,num2 ) =>  (num1 + num2)
+// "Implicit" means something happens automatically, 
+// without being explicitly stated or written.
+// console.log(addtwo(6,5))
+
+
+// const  add = () => { 
+//     return  {username : "Ashiv"}
+// }
+// console.log(add()) 
+
+
+// const  add = () => ({username : "Ashiv"})
+// console.log(add()) 
+
+
+function add(){
+    console.log("Hello world");
+}
+add();
+
+// (function greet(){
+//     console.log("hello ,Ashiv");
+// })();
+
+// //---------------------------------//
+// (() => {
+//     console.log("hello sir")
+// })();
+// //------------------------------//
+// ((name) => {
+//     console.log(`hello ${name}`)
+// })('sam')
+
+//----------------------------------
+((name) => 
+    (console.log(`hello ${name}`))
+)('rahul')
+
