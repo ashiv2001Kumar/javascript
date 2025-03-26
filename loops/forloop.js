53f1904619322ab4ca92106deefe475a4db30121
Ashiv Kumar <131641937+ashiv2001Kumar@users.noreply.github.com>
2025-03-26 06:53:59 +0000

    for loop

---
+++ loops/forloop.js
@@ -0,0 +1,55 @@
+//for
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let i = 0 ; i <= 10; i++){
+//     const element = i;
+//     if(element % 2 == 0){
+//         console.log(element + 'hello')
+//     }
+//     console.log(element)
+// }
+
+// for (let i = 0; i <= 10 ; i++) {
+//     console.log(`outer loop is ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop value: ${j} and inner loop ${i}`)  
+//     }
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value is: ${i}`)
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i + '*' + j + '='+ i*j)
+//     }
+// }
+
+
+// let myarray = ["apple","manago","banana",'lichi']
+// console.log(myarray.length)
+// for (let i = 0; i < myarray.length; i++) {
+//     const element = myarray[i];
+//     console.log(element)
+// }
+
+
+// for(let i = 1 ; i <=20 ; i++){
+//     if (i == 6){
+//         console.log("detected");
+//         break;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+// for(let i = 1 ; i <=20 ; i++){
+//     if (i == 6){
+//         console.log("detected");
+//         continue;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+
